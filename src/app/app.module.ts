a6f2408c94ff21f16d3c5ef6aaec716eeb0c4d63
Balint Horvath <horvath.balint997@gmail.com>
2023-03-09 11:46:24 +0100

    hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { PlayerstatsComponent } from './playerstats/playerstats.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCxiWP5u2M6oYBVKGwl9oeh6pCee6qX2FM',
+  authDomain: 'apex-nexus.firebaseapp.com',
+  projectId: 'apex-nexus',
+  storageBucket: 'apex-nexus.appspot.com',
+  messagingSenderId: '7924265933',
+  appId: '1:7924265933:web:f966726f80610c53530a84',
+  measurementId: 'G-YCETMZ2Y7Q',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 @NgModule({
   declarations: [
     AppComponent,
