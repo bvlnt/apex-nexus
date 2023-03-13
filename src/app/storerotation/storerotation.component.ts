7ab0808d8706572c805bda7df9cf7d48005a42dc
Balint Horvath <horvath.balint997@gmail.com>
2023-03-13 14:35:44 +0100

    countdown to storeitems

--- src/app/storerotation/storerotation.component.ts
+++ src/app/storerotation/storerotation.component.ts
@@ -11,6 +11,8 @@ import { Subscription } from 'rxjs';
 export class StorerotationComponent implements OnInit, OnDestroy {
   storeData!: StoreItem[];
   storeSubscription!: Subscription;
+  countdown: { days: number; hours: number; minutes: number; seconds: number } =
+    { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
   constructor(private storeService: StoreService) {}
 
@@ -20,6 +22,32 @@ export class StorerotationComponent implements OnInit, OnDestroy {
       .subscribe((data) => {
         this.storeData = data;
       });
+
+    //make me a timer that countdowns to tuesday 21:00 every week <div>Expires in: {{countdown.days}}:{{countdown.hours}}:{{countdown.minutes}}</div>
+    setInterval(() => {
+      const now = new Date();
+      const target = new Date();
+      target.setHours(21, 0, 0, 0);
+      if (now.getDay() === 6 && now.getHours() >= 21) {
+        target.setDate(target.getDate() + 7);
+      }
+      while (target.getDay() !== 2) {
+        target.setDate(target.getDate() + 1);
+      }
+      const diff = target.getTime() - now.getTime();
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+      this.countdown = {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+      };
+    }, 1000);
   }
 
   handleImageError(event: ErrorEvent) {
